6bb942b70d6def9f0740acaf68a644fa077ae8c5
ilopez78 <99553835+ilopez78@users.noreply.github.com>
2022-06-24 15:17:23 -0600

    Agregar archivos de proyecto.

---
+++ Tarea3/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Tarea3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+        private async void btnGuardar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var person = new Personas
+                {
+                    nombre = this.nom.Text,
+                    apellido = this.ape.Text,
+                    edad = this.edad.Text,
+                    mail = this.mail.Text,
+                    direccion = this.address.Text
+                };
+
+                var resultado = await App.BD.GrabarPersonas(person);
+
+
+                if (resultado == 1)
+                {
+                    ClearScreen();
+                    await DisplayAlert("Mensaje", "Estos datos han sido guardados exitosamente", "OK");
+                }
+                else
+                {
+                    await DisplayAlert("Mensaje", "Error, No se logró guardar la información", "OK");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Mensaje", ex.Message.ToString(), "OK");
+
+            }
+        }
+
+        //Limpiar pantalla
+        private void ClearScreen()
+        {
+            this.nom.Text = String.Empty;
+            this.ape.Text = String.Empty;
+            this.edad.Text = String.Empty;
+            this.mail.Text = String.Empty;
+            this.address.Text = String.Empty;
+        }
+    }
+
+}
